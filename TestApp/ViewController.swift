8e7717049d6035b8f5a213e0a776a4367bfb71fc
Jakub Vodak <jakub.vodak@strv.com>
2017-09-21 13:38:10 +0200

    Initial Commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Jakub Vodak on 21/09/2017.
+//  Copyright © 2017 Jakub Vodak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Ahoj")
+        
+    }
+
+    
+    
+    @IBAction func mojeFunkce() {
+        
+        print("Jak se mas?")
+    }
+    
+    
+    
+}
+
